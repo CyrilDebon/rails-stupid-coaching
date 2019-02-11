203561165d2bc0080f675a72f125e3679cc72374
CyrilDebon <cyril.debon@gmail.com>
2019-02-11 15:35:38 +0100

    exercice 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask' => 'questions#ask'
+  get 'answer' => 'questions#answer'
+  # root to: 'questions#home'
 end
+
+
+# Rails.application.routes.draw do
+#   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+# end
